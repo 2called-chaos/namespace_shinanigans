a3c8018d9d5d8ada741fd607679da389044d7cd6
Sven Pachnit <sven@bmonkeys.net>
2014-03-25 00:24:20 +0100

    New rails project
    rails new namespace_hell -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NamespaceHell::Application.config.secret_key_base = 'feab826a5337fb4bed773eb3966a0cc11b215a99d1c129039260c0438f7966fd4e70dda4dc62387277f3a9d3673db7d3310dbb798ad00ef37840c68f169bdd22'
